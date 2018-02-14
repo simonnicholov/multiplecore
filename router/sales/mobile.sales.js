08c86d8c096036be987a538d869cff876acc6c6f
Simon <nicholov.simon@alamsutera.co.id>
2018-02-14 18:36:53 +0700

    update notes

--- router/sales/mobile.sales.js
+++ router/sales/mobile.sales.js
@@ -12,6 +12,18 @@ var config = require('../../multiple.config.js');
 console.log(config.setup());
 console.log(config.application() + ' [mobile.sales.js]');
 
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------ //
+// ------------------------------------------------------------------ NOTES ----------------------------------------------------------------------- //
+// ------------------------------------------------------------------------------------------------------------------------------------------------ //
+
+/*
+    When you are using SQL Server 2014 and you've output parameter. ExpressJS doesn't care about output parameter, so it will be treated as input parameter.
+    Please remember, ExpressJS can't return output parameter as a table. So, the behavior between ExpressJS and WebService in C# doesn't have similarity.
+    If you need an output as a table, you have to select manually at the end of your store procedure.
+*/
+
+
 // ================================================================== GET ROUTER ================================================================== //
 
 router.get('/getsingletable', function(req, res) {
